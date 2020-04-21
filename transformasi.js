435ddca499090ffae2590b7f99dba271f5959bbc
siti2503 <61811109+siti2503@users.noreply.github.com>
2020-04-21 23:45:46 +0700

    Trans

--- transformasi.js
+++ transformasi.js
@@ -83,40 +83,7 @@ function draw2() {
     contex.stroke();
     
     // 3
-    contex.save();
-    contex.lineWidth=6;
-    contex.strokeRect(650, 350, panjangPP5, tinggiPP5);
-    
-    contex.translate(500,-53);
-    contex.rotate(Math.PI/2); 
-    contex.translate(-99, -330); 
-    contex.lineWidth=6;
-    contex.strokeRect(389, 65, panjangPP6, tinggiPP6);
-    
-    contex.lineWidth=6;
-    contex.strokeRect(429, 50, panjangPP7, tinggiPP7);
-    contex.restore();
-    
-    contex.beginPath();
-    contex.moveTo(672,470);
-    contex.lineTo(672,380);
-    contex.lineWidth=6;
-    contex.lineCap='round';
-    contex.stroke();
-    
-    contex.beginPath();
-    contex.moveTo(698,470);
-    contex.lineTo(698,380);
-    contex.lineWidth=6;
-    contex.lineCap='round';
-    contex.stroke();
-    
-    contex.beginPath();
-    contex.moveTo(725,470);
-    contex.lineTo(725,380);
-    contex.lineWidth=6;
-    contex.lineCap='round';
-    contex.stroke();
+   
     
     }
     draw2();
