e8f169ee7688a43d7472bc0665a4ecd6b379c466
siti2503 <61811109+siti2503@users.noreply.github.com>
2020-04-21 22:18:49 +0700

    membuat gambar transformasi

---
+++ transformasi.js
@@ -0,0 +1,122 @@
+const myCanvas = document.getElementById('canvas')
+const contex = myCanvas.getContext('2d');
+
+myCanvas.width=myCanvas.scrollWidth;
+myCanvas.height=myCanvas.scrollHeight;
+
+function draw2() {
+
+    contex.save();
+    var panjangPP5 = 100;
+    var tinggiPP5 = 150;
+    contex.lineWidth=6;
+    contex.strokeRect(300, 350, panjangPP5, tinggiPP5);
+    
+    var panjangPP6 = 112;
+    var tinggiPP6 = 15;
+    contex.lineWidth=6;
+    contex.strokeRect(295, 335, panjangPP6, tinggiPP6); 
+    
+    var panjangPP7 = 25;
+    var tinggiPP7 = 15;
+    contex.lineWidth=6;
+    contex.strokeRect(340, 320, panjangPP7, tinggiPP7);
+    contex.restore();
+    
+    contex.beginPath();
+    contex.moveTo(322,470);
+    contex.lineTo(322,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    contex.beginPath();
+    contex.moveTo(348,470);
+    contex.lineTo(348,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    contex.beginPath();
+    contex.moveTo(375,470);
+    contex.lineTo(375,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    
+    
+    // 2
+    contex.save();
+    contex.lineWidth=6;
+    contex.strokeRect(450, 350, panjangPP5, tinggiPP5);
+    
+    contex.translate(561,-68); 
+    contex.rotate((Math.PI/100) * 25); 
+    contex.translate(-27, -50); 
+    contex.lineWidth=6;
+    contex.strokeRect(200, 335, panjangPP6, tinggiPP6);
+    
+    contex.lineWidth=6;
+    contex.strokeRect(244, 320, panjangPP7, tinggiPP7);
+    contex.restore();
+    
+    contex.beginPath();
+    contex.moveTo(473,470);
+    contex.lineTo(473,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    contex.beginPath();
+    contex.moveTo(500,470);
+    contex.lineTo(500,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    contex.beginPath();
+    contex.moveTo(525,470);
+    contex.lineTo(525,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    // 3
+    contex.save();
+    contex.lineWidth=6;
+    contex.strokeRect(650, 350, panjangPP5, tinggiPP5);
+    
+    contex.translate(500,-53);
+    contex.rotate(Math.PI/2); 
+    contex.translate(-99, -330); 
+    contex.lineWidth=6;
+    contex.strokeRect(389, 65, panjangPP6, tinggiPP6);
+    
+    contex.lineWidth=6;
+    contex.strokeRect(429, 50, panjangPP7, tinggiPP7);
+    contex.restore();
+    
+    contex.beginPath();
+    contex.moveTo(672,470);
+    contex.lineTo(672,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    contex.beginPath();
+    contex.moveTo(698,470);
+    contex.lineTo(698,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    contex.beginPath();
+    contex.moveTo(725,470);
+    contex.lineTo(725,380);
+    contex.lineWidth=6;
+    contex.lineCap='round';
+    contex.stroke();
+    
+    }
+    draw2();
